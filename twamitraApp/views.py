94161e908eef0b10c3e42ed834e40900e54bf053
SHIVANSH VASU <shivanshvasuofficial@gmail.com>
2023-10-03 23:44:31 +0530

    Home

--- twamitraApp/views.py
+++ twamitraApp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "home.html")
+
+
+
+def signup(request):
+    return render(request, "signup.html")
 
 
