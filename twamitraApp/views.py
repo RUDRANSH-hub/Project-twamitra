387649270e96488096d66dc269e820acf7aabb0a
Rishiverma-rv <rishiverma.rv.official@gmail.com>
2023-10-06 23:31:38 +0530

    Loan

--- twamitraApp/views.py
+++ twamitraApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return render(request, "home.html")
+
+
+def loan(request):
+    return render(request, "loanForm/loanForm.html")
