6f70209890d730dda08fd96cb0f25060415c0a9f
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-12-06 21:46:35 +0530

    Updated templates and authentication

--- account/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from account.models import account
-@admin.register(account)
-class account_detail(admin.ModelAdmin):
-    list_display = ('username', 'email', 'password')
